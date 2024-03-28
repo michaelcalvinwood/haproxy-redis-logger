c5f9012964abcaca469b11de844622b3d08d3583
Michael Wood <michaelwood33311@icloud.com>
2024-03-28 10:29:46 -0400

    added POST /log endpoint

--- haproxy-redis-logger.js
+++ haproxy-redis-logger.js
@@ -1,11 +1,24 @@
 const httpsPort = 5100;
 const httpPort = 5101;
 
-
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const fs = require('fs');
+const redis = require('redis');
+
+const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
+
+const redisConfig = {
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    password: REDIS_PASSWORD
+}
+
+console.log(redisConfig);
+
+const client = redis.createClient(redisConfig);
 
 const app = express();
 app.use(express.static('public'));
@@ -16,6 +29,9 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.post('/log', async (req, res) => {
+    console.log(req.body);
+})
 
 http.createServer(app).listen(httpPort, '127.0.0.1');
 
