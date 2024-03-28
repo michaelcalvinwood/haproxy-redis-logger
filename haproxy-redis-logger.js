8d33cef3d482f8bd7c524826f42edb71b3165dfd
Michael Wood <michaelwood33311@icloud.com>
2024-03-28 12:42:56 -0400

    added using /log endpoint

--- haproxy-redis-logger.js
+++ haproxy-redis-logger.js
@@ -16,7 +16,7 @@ const redisConfig = {
     password: REDIS_PASSWORD
 }
 
-console.log(redisConfig);
+console.log('redisConfig', redisConfig);
 
 const client = redis.createClient(redisConfig);
 
@@ -25,13 +25,19 @@ app.use(express.static('public'));
 app.use(express.json({limit: '200mb'})); 
 app.use(cors());
 
+app.use((req, res, next) => {
+    //console.log(req.query);
+    next()
+})
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.post('/log', async (req, res) => {
-    console.log(req.body);
+app.get('/log', async (req, res) => {
+    console.log('log', req.query);
+    res.status(200).send('ok');
 })
 
-http.createServer(app).listen(httpPort, '127.0.0.1');
+http.createServer(app).listen(httpPort, '0.0.0.0');
 
